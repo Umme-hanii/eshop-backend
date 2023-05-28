c4f8d08afddfd4c136f5b2aca31af01c2c78b883
Umme-hanii <ummehanimohammed9@gmail.com>
2023-05-28 14:50:57 +0530

    Add express server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello eshop !");
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello API")
+});
+
+app.listen(3004, () => {
+  console.log("Server is running http://localhost:3004")
+});
