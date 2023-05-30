d7589f02bec3cea2e176b4184d72277cf1d6ad6d
Umme-hanii <ummehanimohammed9@gmail.com>
2023-05-30 07:13:50 +0530

    Reading Env variables, Parsing JSON data, Logging API requests

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
-app.get("/", (req, res) => {
-  res.send("Hello API")
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+//Middleware
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.get(`${api}/products`, (req, res) => {
+  const products = {
+    id: 1,
+    name: 'Hair dryer',
+    image: 'some_url'
+  }
+  res.send(products)
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body
+  console.log(newProduct)
+  res.send(newProduct)
 });
 
 app.listen(3004, () => {
+  console.log(api)
   console.log("Server is running http://localhost:3004")
 });
